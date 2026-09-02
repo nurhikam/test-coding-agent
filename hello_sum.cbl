6fef1d31914a4bd2814c1c85964fdcedcc63b622
nurhikam <nurhikam@simplifyai.id>
2026-09-02 16:14:37 +0700

    change to sum even numbers up to 100

--- hello_sum.cbl
+++ hello_sum.cbl
@@ -3,14 +3,16 @@ program-id. hello-sum.
 
 data division.
 working-storage section.
-01  ws-counter       pic 9(2) value 0.
-01  ws-sum           pic 9(4) value 0.
+01  ws-counter       pic 9(3) value 0.
+01  ws-sum           pic 9(5) value 0.
 
 procedure division.
 main-logic.
-    perform varying ws-counter from 1 by 1 until ws-counter > 50
-        add ws-counter to ws-sum
+    perform varying ws-counter from 1 by 1 until ws-counter > 100
+        if function rem(ws-counter, 2) = 0
+            add ws-counter to ws-sum
+        end-if
     end-perform.
 
-    display "The sum of numbers from 1 to 50 is: " ws-sum.
+    display "The sum of even numbers from 1 to 100 is: " ws-sum.
     stop run.
